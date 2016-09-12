bd4c8099d842335b784cd45bfc524bb535ad1fc1
Catalin Enache <catalin.enache@ixaris.com>
2016-09-12 17:08:53 +0300

    added build-prod script

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
-'use strict'
+'use strict';
 
-import 'babel-polyfill'
+import 'babel-polyfill';
 
 // require('./examples/hoc_es6')();
 // require('./examples/basics')();
--- src/examples/table_performance.jsx
+++ src/examples/table_performance.jsx
@@ -1,7 +1,16 @@
 'use strict';
 
-const React = require( 'react' );
-const ReactDOM = require( 'react-dom' );
+
+// console.log(process.env.NODE_ENV);
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+// const React = require( 'react/dist/react.min' ); // react/dist/react.min
+// const ReactDOM = React.__SECRET_DOM_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
+
+
+const shallowEqual = require('../lib/shallowEqual');
 
 let c = 0;
 function generateTableData(rowsNum, colsNum) {
@@ -22,15 +31,18 @@ class App extends React.Component {
     constructor(props, ...args) {
         super(props, ...args);
         this.regenerate = this.regenerate.bind(this);
+        this.updateState = this.updateState.bind(this);
         this.state = {
-            tableData: generateTableData(props.rowsNum, props.colsNum)
+            tableData: generateTableData(props.rowsNum, props.colsNum),
+            fake: true
         }
     }
 
     static get propTypes() {
         return {
             rowsNum: React.PropTypes.number.isRequired,
-            colsNum: React.PropTypes.number.isRequired
+            colsNum: React.PropTypes.number.isRequired,
+            check: React.PropTypes.bool.isRequired
         }
     }
 
@@ -56,12 +68,19 @@ class App extends React.Component {
         });
     }
 
+    updateState() {
+        this.setState({
+            fake: !this.state.fake
+        })
+    }
+
     render() {
         console.log('App render');
         let rows = this.state.tableData.map((row, i) => <Row key={i} idx={i} rowData={row} /> );
         return (
             <div>
                 <button onClick={this.regenerate}>re-generate {this.props.rowsNum} * {this.props.colsNum} </button>
+                <button onClick={this.updateState}>update state {+this.state.fake}</button>
                 <Table>{rows}</Table>
             </div>
         );
@@ -100,7 +119,14 @@ class Row extends React.Component {
         }
     }
 
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return !shallowEqual(this.props, nextProps, null, false) ||
+            !shallowEqual(this.state, nextState, null, false) ||
+            !shallowEqual(this.context, nextContext, null, false);
+    }
+
     render() {
+        // console.log('row render');
         let rowData = this.props.rowData || [];
         let cells = Object.keys(rowData).map((key, i) => <Cell key={i} idx={i} rowIdx={this.props.idx} textData={rowData[key]}> {i}/{this.props.idx} </Cell> );
         return (
